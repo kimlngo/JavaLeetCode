5d1661e4efe486e53e28cb38556dfeb9d8b07950
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-01-31 03:25:45 -0500

    LeetCode Merge String

---
+++ src/com/kimlngo/leetcode/ProblemSolving.java
@@ -0,0 +1,32 @@
+package com.kimlngo.leetcode;
+
+public class ProblemSolving {
+    public static void main(String[] arg) {
+        String word1 = "abcd", word2 = "pq";
+        System.out.println(mergeString(word1, word2));
+    }
+
+    private static String mergeString(String word1, String word2) {
+        int i = 0, j = 0;
+        int length1 = word1.length();
+        int length2 = word2.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i < length1 && j < length2) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+
+            i++;
+            j++;
+        }
+
+        if (length1 == length2) return sb.toString();
+        else if (length1 < length2)
+            return sb.append(word2.substring(j))
+                     .toString();
+        else
+            return sb.append(word1.substring(i))
+                     .toString();
+    }
+}
