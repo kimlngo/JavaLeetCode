11e0c24579c527fb03e4cad4cc932d68416a0729
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-03-15 13:31:28 -0400

    Improve runtime by reducing Auto-boxing

--- src/com/kimlngo/leetcode/ThreeSums.java
+++ src/com/kimlngo/leetcode/ThreeSums.java
@@ -1,44 +1,43 @@
 package com.kimlngo.leetcode;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ThreeSums {
     public static void main(String[] args) {
-        System.out.println(threeSum(new int[]{0,0,0}));
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
     private static List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> sorted = Arrays.stream(nums)
-                                     .sorted()
-                                     .boxed()
-                                     .collect(Collectors.toList());
+        Arrays.sort(nums);
 
         Set<List<Integer>> result = new HashSet<>();
-        int i = 0, j, k, sum;
+        int i = 0, j, k, sum, length = nums.length;
 
-        while (i < sorted.size() - 1 && sorted.get(i) < 1) {
+        while (i < length - 1 && nums[i] < 1) {
             j = i + 1;
-            k = sorted.size() - 1;
+            k = length - 1;
 
             while (j < k) {
-                sum = sorted.get(i) + sorted.get(j) + sorted.get(k);
-                
+                sum = nums[i] + nums[j] + nums[k];
+
                 if (sum == 0) {
-                    result.add(Arrays.asList(sorted.get(i), sorted.get(j), sorted.get(k)));
-                    while (j < sorted.size() - 2 && sorted.get(j) == sorted.get(j + 1)) j++;
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < length - 2 && nums[j] == nums[j + 1]) j++;
                     j++;
-                    while (k >= 1 && sorted.get(k) == sorted.get(k - 1)) k--;
+                    while (k >= 1 && nums[k] == nums[k - 1]) k--;
                     k--;
                 } else if (sum > 0) {
-                    while (k >= 0 && sorted.get(k) == sorted.get(k - 1)) k--;
+                    while (k >= 1 && nums[k] == nums[k - 1]) k--;
                     k--;
                 } else {
-                    while (j < sorted.size() - 2 && sorted.get(j) == sorted.get(j + 1)) j++;
+                    while (j < length - 2 && nums[j] == nums[j + 1]) j++;
                     j++;
                 }
             }
-            while (i < sorted.size() - 2 && sorted.get(i) == sorted.get(i + 1)) i++;
+            while (i < length - 2 && nums[i] == nums[i + 1]) i++;
             i++;
         }
 
