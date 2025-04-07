f347bbbdf602038c1433b5090d680f15bded6017
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-04-07 12:59:54 -0400

    Fix Binary Search Bug

--- src/com/kimlngo/leetcode/SearchInsertionPosition.java
+++ src/com/kimlngo/leetcode/SearchInsertionPosition.java
@@ -3,29 +3,32 @@ package com.kimlngo.leetcode;
 public class SearchInsertionPosition {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6};
-        System.out.println(searchInsert(nums, 2));
-        System.out.println(searchInsert(nums, 4));
+        System.out.println(searchInsert(nums, 1));
+        System.out.println(searchInsert(nums, 3));
+        System.out.println(searchInsert(nums, 5));
+        System.out.println(searchInsert(nums, 6));
     }
 
     public static int searchInsert(int[] nums, int target) {
+        int length = nums.length;
         //corner case
-        if (target > nums[nums.length - 1])
-            return nums.length;
-        else if (target == nums[nums.length - 1])
-            return nums.length - 1;
+        if (target > nums[length - 1])
+            return length;
+        else if (target == nums[length - 1])
+            return length - 1;
         else if (target <= nums[0])
             return 0;
 
-        int start = 0, end = nums.length - 1;
+        int start = 0, end = length - 1;
 
-        while (start < end) {
-            int mid = end - start / 2;
+        while (start < end - 1) {
+            int mid =  (start + end) / 2;
 
             if (nums[mid] == target) return mid;
             else if (target < nums[mid]) {
-                end = mid - 1;
+                end = mid;
             } else {
-                start = mid + 1;
+                start = mid;
             }
         }
         if(nums[start] > target) return start;
